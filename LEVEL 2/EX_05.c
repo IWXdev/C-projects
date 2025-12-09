9ca2a0b716bd69c9457f7dad77452a52a79b02b3
IWXdev <zoroayoub3@gmail.com>
2025-12-09 02:13:52 +0100

    V 0.3

--- LEVEL 2/EX_05.c
+++ LEVEL 2/EX_05.c
@@ -4,18 +4,19 @@ int main()
 {
         int m[4][4];
         int n;
-        int a;
-        int i;
-        int x;
+        int a, i, x, s, diag_sum;
         printf("Enter matrix size : ");
         scanf("%d", &n);
-        while(n > 4)
+        while (n < 1 || n > 4)
         {
-                printf("number is big Enter matrix size : ");
-                scanf("%d", &n);     
+                printf("Size must be between 1 and 4: ");
+                scanf("%d", &n);
         }
+        int k[n];
+        diag_sum = 0;
         i = 0;
         x = 0;
+        s = 0;
         while(i < n)
         {
                 a = 0;
@@ -23,11 +24,23 @@ int main()
                 {
                         printf("Enter element [%d][%d]: ", i, a);
                         scanf("%d", &m[i][a]);
+                        if(i == a)
+                        {
+                                k[s] = m[i][a];
+                                diag_sum += m[i][a];
+                                s++;
+                        }
                         x += m[i][a];
                         a++;
                 }
                 i++;
         }
+        for (int j = 0; j < s; j++)
+        {
+                printf("%d ", k[j]);
+        }
+        printf("\n");
         printf("sum = %d\n", x);
+        printf("diag_sum = %d\n", diag_sum);
         return 0;
 }
